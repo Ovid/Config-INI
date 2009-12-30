96e8043729d90af6d12a901567f602c15632098f
Curtis Poe <ovid@cpan.org>
2009-12-30 17:37:01 +0000

    First pass at a Perl 6 INI parser.

---
+++ lib/Config/INI.pm
@@ -0,0 +1,134 @@
+# vim:ft=perl6
+
+grammar Config::INI::Grammar {
+    token TOP {
+        <root_section>?
+        <section>+
+    }
+    token root_section {
+        [ <property> | <ignore> ]*
+    }
+    regex section {
+        ^^ '[' <.ws> $<name>=<-[\n\]]>*? <.ws> ']' \h* \n
+        [ <property> | <ignore> ]*
+    }
+    regex property {
+        ^^ <.ws> $<name>=[<.print>+?] <.ws> '=' <.ws> $<value>=[\N*?] \h* \n
+    }
+    token ignore {
+        | ^^ <.ws> [';'|'#'] [\N*] \n
+        | ^^ \h* \n
+    }
+}
+
+class Config::INI {
+    has %.sections is rw;
+    has $.file     is rw;
+
+    method read (Str $file) {
+        $.file = $file;
+        self.read_string(slurp($file));
+    }
+
+    # tried to do a multi to avoid the //= but couldn't multi on void sigs
+    method properties (Str $name? is rw) {
+        $name //= '';    
+        if ! %.sections.exists($name) {
+            die "No properties found for ($name)";
+        }
+        return %.sections{$name};
+    }
+    method add_properties(:%properties!, Str :$name = '') {
+        %.sections{$name} = %properties;
+    }
+
+    method read_string(Str $text) {
+        %.sections = ();
+        my $config = Config::INI::Grammar.parse($text);
+
+        # there can only be one
+        if my $root = $config<root_section>[0]<property> {
+            # XXX Why do I have to stringify the value?
+            %.sections{''} = %( $root.map: { ( '' ~ $_<name> => '' ~ $_<value> ) } );
+        }
+
+        for $config<section> -> $section {
+            my $name = $section<name>;
+            my %properties = $section<property>.map: { ( '' ~ $_<name> => '' ~ $_<value>) };
+            %.sections{$name} = %properties;
+        }
+        return 1;
+    }
+}
+
+=begin pod
+
+=head1 NAME
+
+Config::INI - Read INI files
+
+=head1 SYNOPSIS
+
+ my Config::INI $config .= new;
+ $config.read($file);
+
+ for $config.properties.kv -> $k, $v {
+     say "$ky = $v";
+ }
+ for $config.properties($section_name).kv -> $k, $v {
+     say "$ky = $v";
+ }
+
+=head1 DESCRIPTION
+
+This module reads INI files.  Unfortunately, different authors have, over the
+years, implemented their idea of what INI files should be, so we try to be a
+bit tolerant.  Each line in an INI file should match the general form of:
+
+ $key '=' $value
+
+For each key and value, we trim the leading and trailing whitespace (but leave
+embedded whitespace alone).  Lines beginning with a semi-colon (;), a hash
+mark (#) or containing only whitespace are ignored.
+
+Optionally, a "section" may be defined by prepending sets of k/v pairs with a
+name in square brackets.  The following will place subsequent k/v pairs under
+the 'admin user' section (note the leading and trailing whitespace are
+trimmed).
+
+ [ admin user ]
+    ; virtually omnipotent
+    alias = root
+    hair  = fabulous
+
+=head1 METHODS
+
+=head2 Class Methods
+
+=head3 C<new>
+
+ my Config::INI $config .= new;
+
+Takes no arguments. Returns a C<Config::INI> object.
+
+=head2 Instance Methods
+
+=head3 C<read>
+
+ my Config::INI .= new;
+ $config.read($filename);
+
+Attempts to read an INI file.
+
+=head3 C<properties>
+
+ my %root_properties = $config.properties;
+ my %admin           = $config.properties('admin');
+
+Returns a hash of the k/v pairs. If no name is given, returns the root
+properties (properties defined before any section marker).  Otherwise,
+attempts to return a hash of the properties for a given section.
+
+Dies if we attempt to fetch properties for a section not in the INI file.
+
+=end pod
---
+++ t/main.t
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl6
+# vim:ft=perl6
+
+use v6;
+use Test;
+
+use lib 'lib';
+use Config::INI;
+
+ok my Config::INI $config .= new, 'We can create Config::Tiny objects';
+ok $config.can('read'), 'We should have a read() method';
+
+ok $config.can('add_properties'), 'We should be able to add a config section';
+
+my %properties = (
+    this => 'that',
+    foo  => 'bar',
+);
+ok $config.add_properties(:%properties), '... and we should be able to add properties';
+
+ok $config.can('properties'), 'We should be able to fetch properties';
+is_deeply $config.properties, %properties, 
+    '... and they should be the correct properties';
+
+%properties = (
+    one => 'uno',
+    two => 'dos',
+);
+
+ok $config.add_properties( name => 'spanish', :%properties), 
+    'We should be able to add named properties';
+is_deeply $config.properties('spanish'), %properties,
+    '... and fetch them by name';
+
+my $text = Q{
+uno=dos
+tres = quatro
+[ foo bar  ] 
+this=that
+
+ one = two 
+  # ignore this
+   ; and this
+
+};
+
+ok $config.can('read_string'), 'We should have a method to read a config string';
+ok $config.read_string($text), '... and it should successfully read a config string';
+is_deeply $config.properties, { uno => 'dos', tres => 'quatro' },
+    '... and the root properties should be correct';
+is_deeply $config.properties('foo bar'), { this => 'that', one => 'two' },
+    '... as should the named properties';
+
+ok $config.read('t/config.txt'), 'We should be able to read a file';
+is_deeply $config.properties, 
+    { port => "3333", host => "http://localhost/" },
+    '... and root properties should be read correctly';
+is_deeply $config.properties('admin'),
+    { access => 'all', name => 'Administrator' },
+    '... as should named properties';
+dies_ok { $config.properties('none') },
+    '... but it should die if we try to fetch unknown properties';
+
+done_testing;
